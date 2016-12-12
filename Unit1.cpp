55d3380b8449dcad3a1dcacf544f450ba74df424
pusok <pusok-11@mail.ru>
2016-12-12 12:33:19 +0300

    2

--- Unit1.cpp
+++ Unit1.cpp
@@ -1,1 +1,17 @@
+      //---------------------------------------------------------------------------
 
+#include <vcl.h>
+#include <iostream>
+#include <cmath>
+ 
+int main()
+{
+    double R;
+     std::cout << "R=";
+    std::cin >> R;
+ 
+    double S, L, V;
+ 
+    if (R < 0)
+        std::cout << "Error!";
+   
