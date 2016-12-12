e71aad6315e47786406a33b49833ce8b1824d5ec
pusok <pusok-11@mail.ru>
2016-12-12 12:33:51 +0300

    3

--- Unit1.cpp
+++ Unit1.cpp
@@ -14,4 +14,17 @@ int main()
  
     if (R < 0)
         std::cout << "Error!";
-   
+ else
+    {
+        L = 2 * M_PI * R;
+        S = M_PI * R * R;
+        V = 4.0/3 * M_PI * R * R * R;
+
+        std::cout << "L = " << L << std::endl;
+        std::cout << "S = " << S << std::endl;
+        std::cout << "V = " << V << std::endl;
+    }
+
+
+system("PAUSE");
+return 0;    }
